841836d85bfbca40a9645a9530276744ae194d76
asre459 <asre45900@gmail.com>
2024-12-29 03:40:29 -0300

    some change

--- song-backend/server.js
+++ song-backend/server.js
@@ -1,13 +1,12 @@
 
 const express = require('express');
-// import express from 'express'
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const songRoutes = require('./routes/songs');
 const path = require('path');
 const fs= require('fs')
 
-const app = express();
+const app = express()
 const PORT = 5000;
 // Middleware
 app.use(cors());
@@ -18,7 +17,7 @@ app.use((err, req, res, next) => {
 app.use(bodyParser.json());
 // Serve uploaded files in the "database" folder
 app.use('/uploads', express.static(path.join(__dirname, 'database')));
-.98// Routes
+// Routes
 app.use('/api/songs', songRoutes);
 
 app.listen(PORT, () => {
